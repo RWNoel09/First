accf0dd47f5e525b316b251f51fd1e5c7b8f7bab
RWNoel09 <47224683+RWNoel09@users.noreply.github.com>
2019-02-22 20:58:30 -0500

    Update listoutCS.js

--- WebScraperLab/listoutCS.js
+++ WebScraperLab/listoutCS.js
@@ -1,1 +1,6 @@
-var
+var s = document.createElement("script")
+s.src = "https://d3js.org/d3.v5.js"
+var body = document.getElementsByTagName("body")
+var body = document.getElementsByTagName("body")[0]
+body.after(s)
+d3.selectAll("#main ul").nodes().forEach(function(e){console.log(e.innerText)})
