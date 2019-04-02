c7ae25d116415cdc5dfd1b23cd87566680ab2e16
RWNoel09 <47224683+RWNoel09@users.noreply.github.com>
2019-04-02 12:15:53 -0400

    Update code.js

--- penguinproject/code.js
+++ penguinproject/code.js
@@ -19,6 +19,7 @@ var getgradeArray=function(d){
       return hw.grade}) 
     var hwgrade = hwarray.reduce(function(total, amount){
       return ((total + amount)/950)*100})
+    console.log(hwgrade)
     var quizarray = student.quizes.map(function(quiz){
       return quiz.grade})  
     var quizgrade = quizarray.reduce(function(total, amount){
@@ -28,7 +29,6 @@ var getgradeArray=function(d){
     var testgrade = testarray.reduce(function(total, amount){
       return ((total + amount)/200)*100})
     var stugrade = 0.3*finalgrade + 0.4*testgrade + 0.15*quizgrade + 0.15*hwgrade
-    console.log(stugrade)
     return stugrade 
   })
 }
