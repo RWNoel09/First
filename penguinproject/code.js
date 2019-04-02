3009a3abd8fe117e67522658deddf9583de03d10
RWNoel09 <47224683+RWNoel09@users.noreply.github.com>
2019-04-01 23:26:27 -0400

    Update code.js

--- penguinproject/code.js
+++ penguinproject/code.js
@@ -1,4 +1,36 @@
 d3.json('classData.json').then(function(data){
 
-drawHistogram(data)
+drawScatterplot(data)
+console.log(data)
 })
+
+var drawScatterplot = function(data){
+
+var screen={width:650,height:420};
+ var margin = {top: 20, right: 10, bottom: 40, left: 70};
+ var h=screen.height-margin.top-margin.bottom
+ var w=screen.width-margin.right-margin.left
+
+ var gradearray = getgradeArray(data)
+
+
+var getgradeArray=function(d){
+  var gradearray = d.map(function(student){
+    var finalgrade = student.final.grade
+    var hwarray = student.homework.map(function(hw){
+      return hw.grade})  
+    var hwgrade = hwarray.reduce(function(total, amount){
+      return ((total + amount)/950)*100})
+    var quizarray = student.quizes.map(function(quiz){
+      return quiz.grade})  
+    var quizgrade = quizarray.reduce(function(total, amount){
+      return ((total + amount)/380)*100})
+    var testarray = student.test.map(function(t){
+      return t.grade})  
+    var testgrade = testarray.reduce(function(total, amount){
+      return ((total + amount)/200)*100})
+    var stugrade = 0.3*finalgrade + 0.4*testgrade + 0.15*quizgrade + 0.15*hwgrade
+    return stugrade
+  })
+}
+}
