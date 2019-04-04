6ece5fb9a8c6b645316ca941665a94444ca498d1
RWNoel09 <47224683+RWNoel09@users.noreply.github.com>
2019-04-04 15:32:28 -0400

    Update code.js

--- ghibli/code.js
+++ ghibli/code.js
@@ -1,1 +1,36 @@
+dataP = d3.json("https://ghibliapi.herokuapp.com/locations")
+dataP.then(function(data){
+    var column = ["name", "climate", "terrain", "surface_water"]
+    var columnH = ["Name", "Climate", "Terrain", "Surface Water"]
+    drawTable(data, column, columnH)
+});
 
+var drawTable = function (data, column, columnH){
+  var table = d3.select("body").append("table")
+  var thead = table.append("thead")
+  var tbody = table.append("tbody");
+
+  thead.append('tr')
+       .selectAll("th")
+       .data(columnH)
+       .enter()
+       .append("th")
+       .text(function(column){return column;})
+
+  var rows = tbody.selectAll("tr")
+                  .data(data)
+                  .enter()
+                  .append("tr")
+                  .attr("id", "row");
+  var cells = rows.selectAll("td")
+                  .data(function (row) {
+                    return column.map(function(column) {
+                      return { column: column, value: row[column]};
+                    })
+                  })
+                  .enter()
+                  .append("td")
+                  .text(function(d) {return d.value});
+  return table;
+
+}
