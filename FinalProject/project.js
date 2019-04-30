d82f479fb48fb9b55869ed4079a155e5cd200d27
RWNoel09 <47224683+RWNoel09@users.noreply.github.com>
2019-04-30 15:41:54 -0400

    Update project.js

--- FinalProject/project.js
+++ FinalProject/project.js
@@ -5,47 +5,67 @@ Promise.all([mbsP,mdP])
        .then(function(values){
        var mbsData = values[0]
        var mdData = values[1]
-       var date = 0
+       var date = 1
        drawBubble(mbsData,date)
        })
 
 var drawBubble = function(mbsData,date){
-	
+
  var screen={width:800,height:500};
  var margin = {top: 20, right: 60, bottom: 40, left: 70};
  var h=screen.height-margin.top-margin.bottom
  var w=screen.width-margin.right-margin.left
- 
- 
+
+
  console.log(mbsData)
-	
+ var valArray = mbsData.map(function(mbsData){
+   return parseFloat(mbsData.mbs);
+ })
+ var dateArray = mbsData.map(function(mbsData){
+   return mbsData.DATE;
+ })
+
+ var mbsAcum = function(mbsData,date){
+   mbsArray1 = mbsData.slice(0,date+1)
+   console.log(mbsArray1)
+   mbsArray = mbsArray1.map(function(mbsData,i){
+     if (date >= i) {
+              return parseFloat(mbsData.mbs);}
+   })
+   return mbsArray
+}
+
  var svg = d3.select("body")
  	     .append("svg")
  	     .attr("width",w)
              .attr("height",h)
-	
-  var bubble = d3.pie();
-  var mbsVal = mbsData[date]
-  var outerRadius = 1.9452197*mbsVal.mbs;
-  var innerRadius = 0;
-  var arc = d3.arc()
-	      .innerRadius(innerRadius)
-	      .outerRadius(outerRadius)
-  var arcs = svg.selectAll("g.arc")
-		.data(bubble(mbsVal.mbs))
-  		.enter()
-  		.append("g")
-  		.attr("class","arc")
-  		.attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
- arcs.append("path")
-     .attr("fill", "green")
-     .attr("d", arc)
- arcs.append("text")
-    .attr("transform", function(d) {
-         return "translate(" + arc.centroid(d) + ")";})
- .attr("text-anchor","middle")
- .text(function(d) {
-	 return mbsVal.mbs;});
 
- 
+  var mbsTotal = mbsAcum(mbsData,date)
+  console.log(mbsTotal)
+  var bubbleVal = mbsTotal.reduce(function(total, amount){
+            return total + amount})
+  console.log(bubbleVal)
+
+  svg.selectAll("circle")
+     .data()
+     .enter()
+     .append("circle")
+     .attr("cx", 200)
+     .attr("cy", 250)
+     .attr("r", bubbleVal*1.9452197)
+     .attr("fill", "green")
+
+
+   xScale = d3.scaleTime()
+              .domain([
+                d3.min(dateArray, function(d){ return d;}),
+                d3.max(dateArray, function(d){ return d;})
+              ])
+              .range([400,w]);
+
+   yScale = d3.scaleLinear()
+              .domain([0, d3.max(valArray, function(d) {return d;})])
+              .range([h,0]);
+
+
 }
