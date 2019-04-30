d2c7477f005769b7266adced70c34f92b6979818
RWNoel09 <47224683+RWNoel09@users.noreply.github.com>
2019-04-29 21:34:51 -0400

    Update project.js

--- FinalProject/project.js
+++ FinalProject/project.js
@@ -1,1 +1,13 @@
+var mbsP = d3.csv("mbs1.csv")
+var mdP = d3.csv("md.csv")
 
+Promise.all([mbsP,mdP])
+       .then(function(values){
+       var mbsData = values[0]
+       var mdData = values[1]
+       drawBubble(mbsData)
+       })
+
+var drawBubble = function(mbsData){
+  console.log(mbsdata)
+}
